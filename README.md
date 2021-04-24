98f57fe9b8700b2d3e7f2b325b81c1a49416f5f6
SaltyAom <aomkirby123@gmail.com>
2021-04-24 21:22:12 +0700

    :blue_book: feat - 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # changeif
+#### This package is design for [Niku](https://pub.dev/packages/niku) and [Flutter Hook](https://pub.dev/packages/flutter_hooks).
 Flutter Hook's useMemoized as dot notation.
+
+Rebuild widget if the provided value is change (only work with HookWidget and State).
+
+Requirement:
+- Niku
+- Flutter Hook
+
+Example usage
+```dart
+class WithChangeIf extends HookWidget {
+  build(context) {
+    final counter = useState(0);
+
+    return NikuColumn([
+      NikuText("Will not be rebuild ${counter.value}").changeIf(),
+      NikuButton(Text("Counter: ${counter.value}"))
+        ..onPressed(() {
+          counter.value++;
+        }),
+    ]);
+  }
+}
+```
--- lib/changeif.dart
+++ lib/changeif.dart
@@ -4,10 +4,57 @@ import 'package:flutter/material.dart';
 
 import "package:flutter_hooks/flutter_hooks.dart";
 
+import 'package:niku/niku.dart';
+
 extension ChangeIfExtension on Widget {
   T changeIf<T extends Widget>([List<Object?> keys = const <Object>[]]) =>
       useMemoized(
         () => this as T,
         keys,
       );
+}
+
+extension ChangeIfNiku on Niku {
+  Niku changeIf([List<Object?> keys = const <Object>[]]) =>
+      this..builder((child) => child.changeIf(keys));
+}
+
+extension ChangeIfNikuText on NikuText {
+  Niku changeIf([List<Object?> keys = const <Object>[]]) =>
+      Niku(this)..builder((child) => child.changeIf(keys));
+}
+
+extension ChangeIfNikuColumn on NikuColumn {
+  Niku changeIf([List<Object?> keys = const <Object>[]]) =>
+      Niku(this)..builder((child) => child.changeIf(keys));
+}
+
+extension ChangeIfNikuRow on NikuRow {
+  Niku changeIf([List<Object?> keys = const <Object>[]]) =>
+      Niku(this)..builder((child) => child.changeIf(keys));
+}
+
+extension ChangeIfNikuStack on NikuStack {
+  Niku changeIf([List<Object?> keys = const <Object>[]]) =>
+      Niku(this)..builder((child) => child.changeIf(keys));
+}
+
+extension ChangeIfNikuWrap on NikuWrap {
+  Niku changeIf([List<Object?> keys = const <Object>[]]) =>
+      Niku(this)..builder((child) => child.changeIf(keys));
+}
+
+extension ChangeIfNikuButton on NikuButton {
+  Niku changeIf([List<Object?> keys = const <Object>[]]) =>
+      Niku(this)..builder((child) => child.changeIf(keys));
+}
+
+extension ChangeIfNikuIconButton on NikuIconButton {
+  Niku changeIf([List<Object?> keys = const <Object>[]]) =>
+      Niku(this)..builder((child) => child.changeIf(keys));
+}
+
+extension ChangeIfNikuTextField on NikuTextField {
+  Niku changeIf([List<Object?> keys = const <Object>[]]) =>
+      Niku(this)..builder((child) => child.changeIf(keys));
 }
